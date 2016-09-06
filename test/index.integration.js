1a9503c50e08fba1be1bd767b151f182c8779e3a
Joel Purra <code+github@joelpurra.com>
2016-09-06 20:49:15 +0200

    Add a simple integration test for sanity checking

---
+++ test/index.integration.js
@@ -0,0 +1,26 @@
+var MeddelareCounters = require("../lib"),
+    meddelareCountersOptions = {
+        memoryCache: {
+            // Very short timeouts to finish the test faster.
+            goodResultTimeout: 100,
+            badResultTimeout:  100,
+            timeoutResultTimeout: 100,
+        },
+    },
+    meddelareCounters = new MeddelareCounters(meddelareCountersOptions);
+
+// Use your own website here, or a specific page url.
+var url = "https://meddelare.com",
+    networks = [
+        "facebook",
+        "twitter",
+        "googleplus",
+    ];
+
+meddelareCounters.retrieveCounts(url, networks)
+    .then(function(results) {
+        console.log("Success!", results);
+    })
+    .catch(function(err) {
+        console.error("Fail!", err);
+    });
